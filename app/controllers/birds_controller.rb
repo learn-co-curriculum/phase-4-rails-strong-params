83b647ce4ea400d19c6a9c61c89b33d92b99a36a
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-09-28 15:17:01 +0300

    commit tree 895cd9c1dfc2a74ad31921369f3647d864bb696f

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -22,4 +22,17 @@ class BirdsController < ApplicationController
     end
   end
 
+
+  def create
+    bird = Bird.create(params.permit(:name, :species))
+    render json: bird, status: :created
+  end
+
+  private
+  #all methods below here are private
+
+  def bird_params
+    params.permit(:name, :species)
+  end
+
 end
